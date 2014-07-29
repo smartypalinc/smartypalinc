2274c2e1b3e7910a670a480c5b37f28eb624b7cb
Alex Miller <alex@miller.im>
2014-07-29 12:05:29 -0400

    file deletion and minification

--- js/smarty.js
+++ js/smarty.js
@@ -7,7 +7,7 @@
  * Copyright (c) 2014 Rob Flaherty (@robflaherty)
  * Licensed under the MIT and GPL licenses.
  */
-!function(e,t,n){"use strict";var r,a,l,i={minHeight:0,elements:[],percentage:!0,userTiming:!0,pixelDepth:!0},o=e(t),c=[],u=0;e.scrollDepth=function(h){function p(e,t,n,i){l?(dataLayer.push({event:"ScrollDistance",eventCategory:"Scroll Depth",eventAction:e,eventLabel:t,eventValue:1,eventNonInteraction:!0}),h.pixelDepth&&arguments.length>2&&n>u&&(u=n,dataLayer.push({event:"ScrollDistance",eventCategory:"Scroll Depth",eventAction:"Pixel Depth",eventLabel:f(n),eventValue:1,eventNonInteraction:!0})),h.userTiming&&arguments.length>3&&dataLayer.push({event:"ScrollTiming",eventCategory:"Scroll Depth",eventAction:e,eventLabel:t,eventTiming:i})):(r&&(ga("send","event","Scroll Depth",e,t,25,{nonInteraction:1}),h.pixelDepth&&arguments.length>2&&n>u&&(u=n,ga("send","event","Scroll Depth","Pixel Depth",f(n),1,{nonInteraction:1})),h.userTiming&&arguments.length>3&&ga("send","timing","Scroll Depth",e,i,t)),a&&(_gaq.push(["_trackEvent","Scroll Depth",e,t,1,!0]),h.pixelDepth&&arguments.length>2&&n>u&&(u=n,_gaq.push(["_trackEvent","Scroll Depth","Pixel Depth",f(n),1,!0])),h.userTiming&&arguments.length>3&&_gaq.push(["_trackTiming","Scroll Depth",e,i,t,100])))}function g(e){return{"25%":parseInt(.25*e,10),"50%":parseInt(.5*e,10),"75%":parseInt(.75*e,10),"100%":e-5}}function s(t,n,r){e.each(t,function(t,a){-1===e.inArray(t,c)&&n>=a&&(p("Percentage",t,n,r),c.push(t))})}function v(t,n,r){e.each(t,function(t,a){-1===e.inArray(a,c)&&e(a).length&&n>=e(a).offset().top&&(p("Elements",a,n,r),c.push(a))})}function f(e){return(250*Math.floor(e/250)).toString()}function m(e,t){var n,r,a,l=null,i=0,o=function(){i=new Date,l=null,a=e.apply(n,r)};return function(){var c=new Date;i||(i=c);var u=t-(c-i);return n=this,r=arguments,0>=u?(clearTimeout(l),l=null,i=c,a=e.apply(n,r)):l||(l=setTimeout(o,u)),a}}var D=+new Date;h=e.extend({},i,h),e(n).height()<h.minHeight||("function"==typeof ga&&(r=!0),"undefined"!=typeof _gaq&&"function"==typeof _gaq.push&&(a=!0),"undefined"!=typeof dataLayer&&"function"==typeof dataLayer.push&&(l=!0),p("Percentage","Baseline"),o.on("scroll.scrollDepth",m(function(){var r=e(n).height(),a=t.innerHeight?t.innerHeight:o.height(),l=o.scrollTop()+a,i=g(r),u=+new Date-D;return c.length>=4+h.elements.length?void o.off("scroll.scrollDepth"):(h.elements&&v(h.elements,l,u),void(h.percentage&&s(i,l,u)))},500)))}}(jQuery,window,document);
+!function(e,t,n){"use strict";var r,a,l,i={minHeight:0,elements:[],percentage:!0,userTiming:!0,pixelDepth:!0},o=e(t),c=[],u=0;e.scrollDepth=function(h){function p(e,t,n,i){l?(dataLayer.push({event:"ScrollDistance",eventCategory:"Scroll Depth",eventAction:e,eventLabel:t,eventValue:1,eventNonInteraction:!0}),h.pixelDepth&&arguments.length>2&&n>u&&(u=n,dataLayer.push({event:"ScrollDistance",eventCategory:"Scroll Depth",eventAction:"Pixel Depth",eventLabel:f(n),eventValue:1,eventNonInteraction:!0})),h.userTiming&&arguments.length>3&&dataLayer.push({event:"ScrollTiming",eventCategory:"Scroll Depth",eventAction:e,eventLabel:t,eventTiming:i})):(r&&(ga("send","event","Scroll Depth",e,t,(function(){if(t=="Baseline"){return 0;} else {return 25;} })(),{nonInteraction:1}),h.pixelDepth&&arguments.length>2&&n>u&&(u=n,ga("send","event","Scroll Depth","Pixel Depth",f(n),1,{nonInteraction:1})),h.userTiming&&arguments.length>3&&ga("send","timing","Scroll Depth",e,i,t)),a&&(_gaq.push(["_trackEvent","Scroll Depth",e,t,1,!0]),h.pixelDepth&&arguments.length>2&&n>u&&(u=n,_gaq.push(["_trackEvent","Scroll Depth","Pixel Depth",f(n),1,!0])),h.userTiming&&arguments.length>3&&_gaq.push(["_trackTiming","Scroll Depth",e,i,t,100])))}function g(e){return{"25%":parseInt(.25*e,10),"50%":parseInt(.5*e,10),"75%":parseInt(.75*e,10),"100%":e-5}}function s(t,n,r){e.each(t,function(t,a){-1===e.inArray(t,c)&&n>=a&&(p("Percentage",t,n,r),c.push(t))})}function v(t,n,r){e.each(t,function(t,a){-1===e.inArray(a,c)&&e(a).length&&n>=e(a).offset().top&&(p("Elements",a,n,r),c.push(a))})}function f(e){return(250*Math.floor(e/250)).toString()}function m(e,t){var n,r,a,l=null,i=0,o=function(){i=new Date,l=null,a=e.apply(n,r)};return function(){var c=new Date;i||(i=c);var u=t-(c-i);return n=this,r=arguments,0>=u?(clearTimeout(l),l=null,i=c,a=e.apply(n,r)):l||(l=setTimeout(o,u)),a}}var D=+new Date;h=e.extend({},i,h),e(n).height()<h.minHeight||("function"==typeof ga&&(r=!0),"undefined"!=typeof _gaq&&"function"==typeof _gaq.push&&(a=!0),"undefined"!=typeof dataLayer&&"function"==typeof dataLayer.push&&(l=!0),p("Percentage","Baseline"),o.on("scroll.scrollDepth",m(function(){var r=e(n).height(),a=t.innerHeight?t.innerHeight:o.height(),l=o.scrollTop()+a,i=g(r),u=+new Date-D;return c.length>=4+h.elements.length?void o.off("scroll.scrollDepth"):(h.elements&&v(h.elements,l,u),void(h.percentage&&s(i,l,u)))},500)))}}(jQuery,window,document);
 
 // Tocca.js
 /*! Tocca.js v0.0.8 || Gianluca Guarini */
@@ -16,62 +16,58 @@
 // CUSTOM JAVASCRIPT
 
 // Form submission function
-// Requires "_price" variable to be defined on page to properly register Facebook conversions GA events
-function IsEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
-}
 var subs = 0;
 function formSubmit() {
     var _name=$('#name').val();
     var _email=$('#email').val().toLowerCase();
     if(subs===0){
-        valid_email = IsEmail(_email);
-        if((valid_email===false)||_name.length<1) {
+        if(_email.indexOf('@')==-1||_name.length<1) {
             $('#_valid').toggle();
             setTimeout( function(){$('#_valid').fadeOut( "slow" );}, 1500);
             ga('send', 'event', 'Email Form', 'Failed Submission', 'Invalid Name/Email');
         }
         else {
             $('#email-send').text("Sending email...");
-            $.post("/submit-email", { name: _name, email: _email }, function(data) {
-                if (data=="success101") {
-                    subs = subs + 1;
-                    $('#email-send')
-                        .text("Email Submitted!")
-                        .css({"background":"#d3d3d3","color":"#ffffff"});
-
-                    // Facebook Conversion Code for $7.99 Email Submission
-                    window.fb_param = {}; // IMPORTANT TO DEFINE fb_param VARIABLE GLOBALLY
-                    if(_price=='$7.99') { fb_param.pixel_id = '6012033061246'; }
-                    if(_price=='$4.99') { fb_param.pixel_id = '6012028781846'; }
-                    fb_param.value = '0.00';
-                    fb_param.currency = 'USD';
-                    (function(){
-                      var fpw = document.createElement('script');
-                      fpw.async = true;
-                      fpw.src = '//connect.facebook.net/en_US/fp.js';
-                      var ref = document.getElementsByTagName('script')[0];
-                      ref.parentNode.insertBefore(fpw, ref);
-                    })();
-                    //$('#fb_pixel').addClass('fb_pixel_submit')
-                    ga('send', 'event', 'Email Form', 'Successful Submission', _price);
-                    $.get("/pushover");
-
-                } else {
-                    console.log(data);
-                    $('#email-send').text("Sorry... try Again");
-                    setTimeout( (function(){$('#email-send').text("Submit");})() , 2200);
-                    ga('send', 'event', 'Email Form', 'Failed Submission', 'Server Response  != "success101"');
+            $.ajax({
+                type: "POST",
+                url: "https://mandrillapp.com/api/1.0/messages/send.json",
+                data: {
+                    'key': 'kYTvkI3alIJpJo9OJw475w',
+                    'message': {
+                        'from_email': 'info@smartypal.com',
+                        'to': [
+                            {
+                                'email': _email,
+                                'name': _name,
+                                'type': 'to'
+                            }
+                        ],
+                        'bcc_address': 'info@smartypal.com',
+                        'autotext': 'true',
+                        'subject': 'Thanks from SmartyPAL!',
+                        'html': '<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8"/><title>Thanks from SmartyPAL!</title></head><body style="margin:0"><table width="100%" border="0" cellspacing="0" cellpadding="0"><tr><td align="center" valign="top" bgcolor="#ffffff" style="background-color:#ffffff;"><br><br><table width="600" border="0" cellspacing="0" cellpadding="0"><tr><td align="center" valign="top" style="padding-left:13px;padding-right:13px;background-color:#ffffff;" ><table width="100%" border="0" cellspacing="0" cellpadding="0"><tr><td align="center" valign="middle" style="font-family: sans-serif;font-size:48px;"><a href="http://smartypal.com"><img src="http://smartypal.com.s3-website-us-east-1.amazonaws.com/img/logo-long.png" width="300px" alt="SmartyPAL Logo"/></a></td></tr><tr><td align="center" valign="middle" style="padding-top:7px;"><table width="240" border="0" cellspacing="0" cellpadding="0"><tr><td height="22" align="center" valign="middle" bgcolor="#FEBE00" style="font-family: Arial, sans-serif;font-size:13px;color:#ffffff;margin: 0;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Play&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Apply&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Learn&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr></table></td></tr><tr><td align="left" valign="middle" style="font-family: sans-serif;color:#000000;font-size:18px;padding-top: 40px;"> Thanks for your interest in SmartyPAL! This email confirms your email list sign-up. We will only email you with the latest happenings at SmartyPAL. We promise to not spam you or share your information with third parties. <br><br>If you haven\'t already, be sure to check out our FREE iPad app, <a href="http://bit.ly/1qgWLk">ZooPAL - A Gift</a>. <br><div style="padding-top:25px;"><a href="http://bit.ly/1qgWLk3"><img src="http://smartypal.com.s3-website-us-east-1.amazonaws.com/img/zoopal.png" height="250" style="display:block;margin: 0 auto;"></a></div><br>Thanks for joining us! <br><br><div style="font-size: 15px !important;width: 160px;border-top: 1px solid #000000;margin-top: 10px;padding-top: 5px;">Sincerely,<br>The SmartyPAL Team</div></td></tr><tr><tr><td align="left" valign="middle" style="font-family: sans-serif;font-size:12px;color:#000000;"></table><br><br></td></tr><tr><td align="center" valign="top">&nbsp;</td></tr></table></body></html>'
+                    }
                 }
             }).fail(function() {
                 $('#email-send').text("Sorry... Try Again");
                 setTimeout(function(){$('#email-send').text("Submit");} , 2200);
-                ga('send', 'event', 'Email Form', 'Failed Submission', 'Server Failed to Respond');
+                ga('send', 'event', 'Email Form', 'Failed Submission', 'Mandrill Failed to Respond');
+            }).done(function(response) {
+               if(response[0]["status"]=="sent"){
+                    subs = subs + 1;
+                    $('#email-send')
+                        .text("Email Submitted!")
+                        .css({"background":"#d3d3d3","color":"#ffffff"});
+                    ga('send', 'event', 'Email Form', 'Successful Submission');
+               } else {
+                    console.log(response);
+                    $('#email-send').text("Sorry... try Again");
+                    setTimeout( (function(){$('#email-send').text("Submit");})() , 2200);
+                    ga('send', 'event', 'Email Form', 'Failed Submission', 'Mandrill Response  != "sent"');
+               }
             });
         }
     }
-
 }
 
 ////// Universal Analytics
@@ -80,8 +76,10 @@ function formSubmit() {
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-42710923-2', 'smartypal.com');
+ga('create', 'UA-42710923-1', 'smartypal.com');
+ga('set','allowAnchor',true);
 ga('send', 'pageview');
+window.location.hash = '';
 
 // Scroll depth events (requires external library)
 $(function() {
@@ -94,6 +92,21 @@ ga('send', 'event', 'Trigger Event', 'trigger', 'metric1: Time on Page', { 'metr
 setInterval(function(){ timeOnPage += 10000 ;ga('send', 'event', 'Trigger Event', 'trigger', 'metric1: Time on Page', { 'metric1': timeOnPage/1000 }); console.log(timeOnPage/1000) }, 10000);
 */
 
+// Track outbound links
+var outbounds = $("a").filter(function(index){
+    try{
+        var h = $(this).attr("href");
+        return h.indexOf("http") > -1 && h.indexOf("smartypal.com") == -1;
+    } catch(err){
+        return false;
+    }
+});
+$(outbounds).click(function(){
+    ga('send', 'event', 'Outbound Link', $(this).attr("href"), document.URL );
+    console.log($(this).attr("href"))
+});
+
+
 
 // Event listeners
 $('#email-send').on('tap', function(e,d){
@@ -104,19 +117,6 @@ $("#top-link").click(function() {
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
 });
-$(".call-to-action .signupCTA").on('tap', function() {
-    $('html, body').animate({ scrollTop: $("#signup_form").offset().top - 25 }, "slow");
-    ga('send', 'event', 'Calls to Action', '1 MONTH FREE TRIAL Button');
-    return false;
-});
-$("#free-trial-link").click(function() {
-    $("html, body").animate({ scrollTop: $("#signup_form").offset().top - 25 }, "slow");
-    return false;
-});
-$("#features-link").click(function() {
-    $("html, body").animate({ scrollTop: $("#features").offset().top }, "slow");
-    return false;
-});
 $("#contact-link").click(function() {
     $("html, body").animate({ scrollTop: $("#contact").offset().top }, "slow");
     return false;
@@ -125,8 +125,11 @@ $(".top-bar .logo a").on('tap', function() {
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
 });
+$("#main-text a").click(function() {
+    ga('send', 'event', 'Call to Action', 'Download ZooPAL Link');
+});
 $("#privacy-link").on('tap', function() {
-    $('#privacy-popup div').html('<iframe id="privacy-frame" src="/privacy" width="100%" height="100%"></iframe>');
+    $('#privacy-popup div').html('<iframe id="privacy-frame" src="/privacy.html" width="100%" height="100%"></iframe>');
     $('#privacy-popup').toggle();
     ga('send', 'event', 'Footer Link', 'Privacy and Terms');
 });
@@ -135,7 +138,7 @@ $("#privacy-x").on('tap', function() {
     $('#privacy-popup').hide();
 });
 $("#credits-link").on('tap', function() {
-    $('#credits-popup div').html('<iframe id="credits-frame" src="/credits" width="100%" height="100%"></iframe>');
+    $('#credits-popup div').html('<iframe id="credits-frame" src="/credits.html" width="100%" height="100%"></iframe>');
     $('#credits-popup').toggle();
     ga('send', 'event', 'Footer Link', 'Credits');
 });
