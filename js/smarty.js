265d35f1f1a37c190aae7261ec3ef47b8d6eb83f
alexpmil <alexpmil@gmail.com>
2014-10-11 17:28:23 +0000

    new logo

--- js/smarty.js
+++ js/smarty.js
@@ -13,6 +13,24 @@
 /*! Tocca.js v0.0.8 || Gianluca Guarini */
 !function(a,b){"use strict";if("function"!=typeof a.createEvent)return!1;var c,d,e,f,g,h="undefined"!=typeof jQuery,i=!!("ontouchstart"in window)&&navigator.userAgent.indexOf("PhantomJS")<0,j=function(a,b,c){for(var d=b.split(" "),e=d.length;e--;)a.addEventListener(d[e],c,!1)},k=function(a){return a.targetTouches?a.targetTouches[0]:a},l=function(b,e,f,g){var i=a.createEvent("Event");if(g=g||{},g.x=c,g.y=d,g.distance=g.distance,h)jQuery(b).trigger(e,g);else{i.originalEvent=f;for(var j in g)i[j]=g[j];i.initEvent(e,!0,!0),b.dispatchEvent(i)}},m=!1,n=b.SWIPE_TRESHOLD||80,o=b.TAP_TRESHOLD||200,p=b.TAP_PRECISION/2||30,q=0;i=b.JUST_ON_TOUCH_DEVICES?!0:i,j(a,i?"touchstart":"mousedown",function(a){var b=k(a);e=c=b.pageX,f=d=b.pageY,m=!0,q++,clearTimeout(g),g=setTimeout(function(){e>=c-p&&c+p>=e&&f>=d-p&&d+p>=f&&!m&&l(a.target,2===q?"dbltap":"tap",a),q=0},o)}),j(a,i?"touchend":"mouseup",function(a){var b=[],g=f-d,h=e-c;if(m=!1,-n>=h&&b.push("swiperight"),h>=n&&b.push("swipeleft"),-n>=g&&b.push("swipedown"),g>=n&&b.push("swipeup"),b.length)for(var i=0;i<b.length;i++){var j=b[i];l(a.target,j,a,{distance:{x:Math.abs(h),y:Math.abs(g)}})}}),j(a,i?"touchmove":"mousemove",function(a){var b=k(a);c=b.pageX,d=b.pageY})}(document,window);
 
+
+////// Universal Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-42710923-1', 'smartypal.com');
+
+var campaign_id = String(window.location.hash.replace('#',''));
+if(campaign_id.length>0){
+    console.log("Campaign Id: " + campaign_id);
+    ga('send', 'pageview', {'dimension1':  campaign_id});
+}
+else{
+    ga('send', 'pageview');
+}
+
 // CUSTOM JAVASCRIPT
 
 // Form submission function
@@ -71,7 +89,6 @@ function formSubmit() {
 }
 
 
-var campaign_id = window.location.hash.replace('#','');
 var subs = 0;
 function formSubmit2() {
     var _email=$('#email2').val().toLowerCase();
@@ -121,11 +138,9 @@ function formSubmit2() {
                     }
                     })();
                     window._fbq = window._fbq || [];
-                    window._fbq.push(['track', String(campaign_id), {'value':'0.01','currency':'USD'}]);
-                    $('#email-send2')
-                        .text("Email Submitted!")
-                        .css({"background":"#d3d3d3","color":"#ffffff"});
-                    ga('send', 'event', 'Email Form', 'Successful Submission');
+                    window._fbq.push(['track', campaign_id, {'value':'0.01','currency':'USD'}]);
+                    $('#launch-form').replaceWith('<p style="color: #4e97cc;margin-top: 4%;">Success!<br>Thanks for your interest in SmartyPal.</p>');
+                    ga('send', 'event', 'Email Form', 'Successful Submission', campaign_id);
                } else {
                     console.log(response);
                     $('#email-send2').text("Sorry... try Again");
@@ -136,16 +151,6 @@ function formSubmit2() {
         }
     }
 }
-
-////// Universal Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-42710923-1', 'smartypal.com');
-ga('set','allowAnchor',true);
-ga('send', 'pageview');
 
 // Scroll depth events (requires external library)
 $(function() {
@@ -181,6 +186,7 @@ $('#email-send').on('tap', function(e,d){
 });
 
 $('#email-send2').on('tap', function(e,d){
+    console.log("click");
     e.preventDefault();
     formSubmit2();
 });
